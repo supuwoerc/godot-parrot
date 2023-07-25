55326647869ffe220fd25c35f51b60d46ef1776c
张启蒙 <qimeng_zhang@intsig.net>
2023-07-25 17:41:08 +0800

    timer

--- move.gd
+++ move.gd
@@ -1,14 +1,19 @@
-extends Sprite2D
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var s2_child = Sprite2D.new()
-	add_child(s2_child)
-	print("s2_child is added")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_timer_timeout():
+	var s1 = $s1
+	s1.position.x+=10
+	s1.position.y+=10
+	pass # Replace with function body.
+ 
