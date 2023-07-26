ea2a09701edec17cdcfdc2f851a2770b76dfaa76
张启蒙 <qimeng_zhang@intsig.net>
2023-07-26 20:25:16 +0800

    custom signal

--- move.gd
+++ move.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Timer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,10 +10,20 @@ func _ready():
 func _process(delta):
 	pass
 
+signal customSignal
+var moveCount:int = 0
 
-func _on_timer_timeout():
-	var s1 = $s1
+func _on_timeout():
+	print("timer_timeout")
+	var s1 = get_parent().get_node("s1")
 	s1.position.x+=10
 	s1.position.y+=10
+	moveCount+=1
+	if moveCount>3:
+		emit_signal("customSignal")
+	print(moveCount)
 	pass # Replace with function body.
- 
+
+func _on_custom_signal():
+	print("customSignal reciver")
+	pass # Replace with function body.
